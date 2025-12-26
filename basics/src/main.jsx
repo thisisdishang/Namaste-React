615da8c19e1ade0e4d4b3a1424268f1320fbee7d
thisisdishang <dishangkumarrana@gmail.com>
2025-12-26 19:08:53 +0530

    Create basic components

--- basics/src/main.jsx
+++ basics/src/main.jsx
@@ -1,8 +1,11 @@
+// named import
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// default import
 import App from './App.jsx'
 
+// StrictMode => Highlight deprecated features of React
+// Dev Server => Two times it would render the component
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
